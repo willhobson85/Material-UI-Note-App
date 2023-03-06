647fc7d55045464060c90751654bbb95425eb15c
William Hobson <will.hobson85@gmail.com>
2023-03-06 18:07:24 -0500

    Add typography component to the Create file

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
 
 export default function Create() {
   return (
     <div>
-      Create page
+     <Typography
+      variant='h6'
+      // This will set the style as an h6
+      color='textSecondary'
+      // Sets the text to the secondary color of the theme's text
+      component='h2'
+      // This will make the inspect tool think it is an h2
+      gutterBottom
+      // Adds a margin to the bottom
+     >
+       Create a new note
+     </Typography>
     </div>
   )
 }
