7da2cd43fa075fdc42f06b1d1e5c55bbdf020607
William Hobson <will.hobson85@gmail.com>
2023-03-15 17:34:06 -0400

    Add pages to drawer, style and navigate to appropriate page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { Drawer, Typography, Box } from "@mui/material";
+import { useHistory, useLocation } from "react-router-dom";
+import { Box, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import SubjectOutlinedIcon from '@mui/icons-material/SubjectOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 const drawerWidth = 240;
 
 export default function Layout({ children }) {
+  const history = useHistory()
+  const location = useLocation()
+
+  const menuItems = [
+    {
+      text: 'My Notes',
+      icon: <SubjectOutlinedIcon color='secondary'/>,
+      path: '/'
+    },
+    {
+      text: 'Create Note',
+      icon: <AddCircleOutlineOutlinedIcon color='secondary'/>,
+      path: '/create'
+    }
+  ]
+
   return (
     <Box sx={{ display: 'flex' }}>
       {/* app bar */}
@@ -34,6 +53,19 @@ export default function Layout({ children }) {
             Notes
           </Typography>
         </div>
+        <List>
+          {menuItems.map(item => (
+            <ListItem
+              button
+              key={item.text}
+              onClick={() => history.push(item.path)}
+              sx={location.pathname === item.path ? { backgroundColor: '#f4f4f4' } : null}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
 
     </Box>
